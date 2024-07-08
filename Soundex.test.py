480aa27d5dd4fb04981d964a8e1dfaf31838bb71
Uday612 <69905522+Uday612@users.noreply.github.com>
2024-07-08 12:02:30 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -8,6 +8,12 @@ class TestSoundex(unittest.TestCase):
 
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
+        
+    def test_string_with_special_characters(self):
+        self.assertEqual(generate_soundex("1@!Abc3"),"1120")
+        
+    def test_string_with_vowels(self):
+        self.assertEqual(generate_soundex("Robert"),"R163")
   
 if __name__ == '__main__':
     unittest.main()
