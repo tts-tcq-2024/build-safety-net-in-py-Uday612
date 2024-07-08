99be76ea9b953e247446e5b7c939abe5ea73115d
Uday612 <69905522+Uday612@users.noreply.github.com>
2024-07-08 11:19:53 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -8,7 +8,7 @@ class TestSoundex(unittest.TestCase):
 
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
-
-    
+  
 if __name__ == '__main__':
     unittest.main()
+
